6d610e96faff2ffe351a52b686a0ef024e32d26f
James Officer <james.d.officer@gmail.com>
2023-03-06 09:04:45 +1100

    Add harpoon

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require("config.lazy")
 ----------------
 -- Which Key
 local wk = require("which-key")
+local hm = require("harpoon.mark")
 
 wk.register({
   t = {
@@ -14,6 +15,14 @@ wk.register({
       "<cmd>:ToggleTerm size=" .. tostring(0.3 * vim.o.columns) .. " direction=vertical<cr>",
       "Open Vertical Terminal",
     },
+  },
+}, { prefix = "<leader>" })
+
+wk.register({
+  h = {
+    name = "Harpoon",
+    t = { "<cmd>lua require('harpoon.mark').toggle_file()<cr>", "Toggle file" },
+    l = { "<cmd>Telescope harpoon marks<cr>", "List all marked files" },
   },
 }, { prefix = "<leader>" })
 
