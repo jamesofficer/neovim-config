5eaf3b3d921755d9eefdfe099163117d2dd1b6e6
James Officer <james.d.officer@gmail.com>
2023-03-05 16:48:28 +1100

    Add ToggleTerm and bindings

--- init.lua
+++ init.lua
@@ -1,2 +1,28 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+----------------
+-- Which Key
+local wk = require("which-key")
+
+wk.register({
+  t = {
+    name = "Terminal",
+    t = { "<cmd>:ToggleTermToggleAll<cr>", "Toggle All Terminals" },
+    f = { "<cmd>:ToggleTerm direction=float<cr>", "Open Floating Terminal" },
+    v = {
+      "<cmd>:ToggleTerm size=" .. tostring(0.3 * vim.o.columns) .. " direction=vertical<cr>",
+      "Open Vertical Terminal",
+    },
+  },
+}, { prefix = "<leader>" })
+
+----------------
+-- Null LS
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+  },
+})
